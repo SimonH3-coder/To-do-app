7f76dfe95e2c8a9074c9b5db2188143703f6465e
SimonH3-coder <simo16f4@elev.techcollege.dk>
2025-08-29 10:09:48 +0200

    Add data information structure and save function call

--- _Files/Assets/js/site.js
+++ _Files/Assets/js/site.js
@@ -5,3 +5,11 @@ const colorInput = document.getElementById("colorInput");
 const deleteInput = document.getElementById("deleteInput");
 const colorInput2 = document.getElementById("colorInpu2");
 const saveButton = document.getElementById("saveButton");
+
+const datainformation = {
+  information1: { name: "Indkøb", eddith: "skriv", color: "green", delete: "X", color2: "red" },
+  information2: { name: "Priser", eddith: "skrive", color: "green", delecte: "X", color2: "red" },
+  information3: { name: "Spil", eddith: "skrive", color: "green", delecte: "X", color2: "red" },
+};
+
+saveBruger(datainformation);
