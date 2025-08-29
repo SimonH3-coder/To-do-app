b4bc1dc9237b4c81c4696e761695430cecbb5f75
SimonH3-coder <simo16f4@elev.techcollege.dk>
2025-08-29 12:38:35 +0200

    Fix variable name typo and enhance save functionality in site.js; update HTML structure for user selection

--- Assets/js/site.js
+++ Assets/js/site.js
@@ -2,19 +2,19 @@ const userSelect = document.getElementById("userSelect");
 const nameInput = document.getElementById("nameInput");
 const eddithInput = document.getElementById("eddithInput");
 const colorInput = document.getElementById("colorInput");
-const deleteInput = document.getElementById("deleteInput");
+const delecteInput = document.getElementById("deleteInput");
 const colorInput2 = document.getElementById("colorInpu2");
 const saveButton = document.getElementById("saveButton");
 
 const datainformation = {
-  information1: { name: "Indkøb", eddith: "skriv", color: "green", delete: "X", color2: "red" },
+  information1: { name: "Indkøb", eddith: "skriv", color: "green", delecte: "X", color2: "red" },
   information2: { name: "Priser", eddith: "skrive", color: "green", delecte: "X", color2: "red" },
   information3: { name: "Spil", eddith: "skrive", color: "green", delecte: "X", color2: "red" },
 };
 
 const
 
-saveBruger(datainformation);
+saveInformation(dataInformation);
 
 // Event listener: vis data for valgt bruger
 userSelect.addEventListener("change", userSelectChange);
@@ -41,4 +41,30 @@ function henteinformation() {
   const information = localStorage.getItem("information");
   return JSON.parse(information);
 }
+
+// Event listener: gem data fra formularen
+saveButton.addEventListener("click", () => {
+  console.log("save data");
+
+  const selectedUser = userSelect.value;
+  const name = nameInput.value;
+  const eddith = eddithInput.value;
+  const color = colorInput.value;
+  const delecte = delecteInput.value;
+  const color2 = colorInput2.value;
+
+  // TODO: Gem data i localStorage for den valgte bruger
+  const information = henteinformation();
+  const data = information[selectedUser];
+  console.log(data);
+  data.name = name;
+  data.eddith = eddith;
+  data.color = color;
+  data.delecte = delecte;
+  data.color2 = color2;
+  saveInformation(information);
+});
+
+// Vis data for første bruger ved load
+window.onload = userSelectChange;
 
