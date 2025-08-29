bd420ea5982cba902b523992e815ddd2a37188a1
SimonH3-coder <simo16f4@elev.techcollege.dk>
2025-08-29 09:30:37 +0200

    blue2

---
+++ _Files/Assets/js/site.js
@@ -0,0 +1,7 @@
+const userSelect = document.getElementById("userSelect");
+const nameInput = document.getElementById("nameInput");
+const eddithInput = document.getElementById("eddithInput");
+const colorInput = document.getElementById("colorInput");
+const deleteInput = document.getElementById("deleteInput");
+const colorInput2 = document.getElementById("colorInpu2");
+const saveButton = document.getElementById("saveButton");
