57d24571a60e12f2743270563585bdf6b72f06f3
SimonH3-coder <simo16f4@elev.techcollege.dk>
2025-09-05 11:24:28 +0200

    Refactor initApp function and enhance data handling logic for improved initialization

--- Assets/js/site2.js
+++ Assets/js/site2.js
@@ -1,14 +1,78 @@
 //#region globals
+const contentSection = document.getElementById("content");
+let currentData = null;
+
+//#endregion
+initApp();
+//#endregion
+//#region model code
+function getData() {
+  console.log("getData");
+
+  return JSON.parse(localStorage.getItem("toDoApp_v1"));
+}
+
+function saveData(mydData) {
+  console.log("saveData");
+  let seriallizedData = JSON.stringify(mydData);
+
+  localStorage.setItem("toDoApp_v1", seriallizedData);
+}
+function makeNewData() {
+  console.log("makeNewData");
+  // dummt data hsk at tømme lister inden deployment
+  let newData = {
+    darkMode: false,
+    lists: [
+      {
+        listName: "list 1",
+        items: [
+          { name: "item 1", done: false },
+          { name: "item2", done: true },
+          { name: "item3", done: false },
+        ],
+      },
+      {
+        listName: "indkøb",
+        items: [
+          { name: "kød", done: false },
+          { name: "salat", done: true },
+          { name: "sovs", done: false },
+        ],
+      },
+    ],
+  };
+  //   console.log(newData.lists[0].items[1]);
+
+  return newData;
+}
 
 //#endregion model code
 
 //#region controller code
+function initApp() {
+  console.log("initApp");
+  //hent data
+  currentData = getData();
 
-//#endregion globals
+  // evaluer data
+  if (currentData == null) {
+    // vi har ikke data
+    currentData = makeNewData();
+    saveData(currentData);
+  } else {
+    // Vis data til bruger
+  }
+
+  //evaluere data
+  currentData = getData();
+}
+
+//#endregion controller code
 
 //#region view code
 
-//#endregion globals
+//#endregion view code
 
 //#region globals
 
