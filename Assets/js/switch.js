f94d6f5312848c428863ac9b0b0be20464d2f57b
SimonH3-coder <simo16f4@elev.techcollege.dk>
2025-09-10 09:16:29 +0200

    black32

--- Assets/js/switch.js
+++ Assets/js/switch.js
@@ -1,25 +1,4 @@
 
-//#region init functions
-let appstate2 = "listView";
-let activelist2 = 0;
-currentData ="null";
-
-const dummyData = {
-    darkmode: false,
-    lists: [
-        {
-            id: 1,
-            name: "List 1"
-            items: [
-                {id: 1
-
-                }
-
-            ]
-        }
-
-    ]
-}
 
 
 
@@ -33,21 +12,4 @@ const dummyData = {
 
 
 
-function listClickCallback(action, index) {
-  activelist2 = index;
-  switch (action) {
-    case "showList":
-      listItemView();
-      console.log("showList callback for index:", activelist2);
-      break;
-    case "editList":
-      console.log("editList callback for index:", activelist2);
-      break;
-    case "deleteList":
-      console.log("deleteList callback for index:", activelist2);
-      break;
-    default:
-      console.error("Unknown action:", action);
-      break;
-  }
-}
+
