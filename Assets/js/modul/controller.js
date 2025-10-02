6c43bfcea35fcb6b7472f6a645be6b9db40cf31a
SimonH3-coder <simo16f4@elev.techcollege.dk>
2025-10-02 10:54:17 +0200

    Add button styles for footer and light theme; implement completed state styles in _partials-buttons.scss and _partials-sections.scss

---
+++ Assets/js/modul/controller.js
@@ -0,0 +1,89 @@
+//#region globals
+const contentSection = document.getElementById("content");
+let currentData = null;
+
+//#endregion
+initApp();
+//#endregion
+//#region model code
+function getData() {
+  console.log("getData");
+
+  return JSON.parse(localStorage.getItem("toDoApp_v1"));
+}
+
+function saveData(mydData) {
+  console.log("saveData");
+  let seriallizedData = JSON.stringify(mydData);
+
+  localStorage.setItem("toDoApp_v1", seriallizedData);
+}
+
+function makeNewData() {
+  console.log("makeNewData");
+  // dummt data husk at tømme lister inden deployment
+  let newData = {
+    darkMode: false,
+    lists: [
+      {
+        listName: "list 1",
+        items: [
+          { name: "item1 indkøb", done: false },
+          { name: "item2 priser", done: true },
+          { name: "item3 spil", done: false },
+        ],
+      },
+      {
+        listName: "indkøb",
+        items: [
+          { name: "Ost", done: false },
+          { name: "Mælk", done: true },
+          { name: "Æg", done: false },
+        ],
+      },
+      {
+        listName: "indkøb2",
+        items: [
+          { name: "write", done: false },
+          { name: "wrong", done: true },
+          { name: "right", done: false },
+        ],
+      },
+      {
+        listName: "priser",
+        items: [
+          { name: "100 kr", done: false },
+          { name: "17 kr", done: true },
+          { name: "22 kr", done: false },
+        ],
+      },
+      {
+        listName: "priser2",
+        items: [
+          { name: "write", done: false },
+          { name: "wrong", done: true },
+          { name: "right", done: false },
+        ],
+      },
+      {
+        listName: "spil",
+        items: [
+          { name: "Sly 2", done: false },
+          { name: "Diablo 2", done: true },
+          { name: "DOOM 4", done: false },
+        ],
+      },
+      {
+        listName: "spil2",
+        items: [
+          { name: "write", done: false },
+          { name: "wrong", done: true },
+          { name: "right", done: false },
+        ],
+      },
+    ],
+  };
+  //   console.log(newData.lists[0].items[1]);
+
+  return newData;
+}
